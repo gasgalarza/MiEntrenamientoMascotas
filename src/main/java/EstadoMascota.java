7551f17454765fc53d41287d0f5f59319f8beee3
Gaston <gas.galarza14@gmail.com>
2018-04-09 01:15:50 -0300

    cambios

---
+++ src/main/java/EstadoMascota.java
@@ -0,0 +1,6 @@
+
+public interface EstadoMascota {
+	public void avanzarHasta(int x, int y, Mascota unaMascota);
+	public void descansar(Mascota unaMascota);
+	public void cambiarDeEstadoA(Mascota unaMascota);
+}
